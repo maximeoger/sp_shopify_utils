17119888e84ef0c49a1f1c24b87c31c13d63136b
maximeoger <maximeoger93@proton.me>
2023-04-12 09:24:19 +0200

    Add working SQL file as Base, and add export for Necromunda

--- export_base.sql
+++ export_base.sql
@@ -1,22 +1,16 @@
 SELECT SQL_CALC_FOUND_ROWS p.`id_product`  AS `id_product`,
- pl.`link_rewrite` AS `Handle`,
- pl.`name` AS `Title`,
- pl.`meta_title` AS `Meta Title`,
- pl.`meta_description` AS `Meta Description`,
- pl.`description` AS `Body (HTML)`,
- p.`reference`  AS `Sku`,
- p.`price`  AS `Price / International`,
- 'active'  AS `Status`,
- 'Jeux De Figurines' AS `Custom product type`,
- 'Games Workshop'  AS `Vendor`,
- CONCAT('https://www.starplayer.fr/',i.id_image,'-large_default/',pl.link_rewrite,'.jpg') AS `Image Src`,
- '1' AS `Image Position`,
- '' AS `Barcode`,
- 'FALSE'  AS `is_virtual`,
- il.`legend` AS `Image Alt Text`,
- 0 AS `Compare At Price / International`,
- 'TRUE' AS `Requires Shipping`,
- 'TRUE' AS `Taxable`,
+ p.`reference`  AS `reference`,
+ p.`price`  AS `price`,
+ p.`id_shop_default`  AS `id_shop_default`,
+ p.`is_virtual`  AS `is_virtual`,
+ pl.`name`  AS `name`,
+ pl.`link_rewrite`  AS `link_rewrite`,
+ sa.`active`  AS `active`,
+ shop.`name`  AS `shopname`,
+ image_shop.`id_image`  AS `id_image`,
+ cl.`name`  AS `name_category`,
+ 0 AS `price_final`,
+ pd.`nb_downloadable`  AS `nb_downloadable`,
  sav.`quantity`  AS `sav_quantity`,
  IF(sav.`quantity`<=0, 1, 0) AS `badge_danger`,
  cp.`position`  AS `position` 
@@ -29,7 +23,6 @@ FROM  `product` p
  LEFT JOIN `shop` shop ON (shop.id_shop = 1) 
  LEFT JOIN `image_shop` image_shop ON (image_shop.`id_product` = p.`id_product` AND image_shop.`cover` = 1 AND image_shop.id_shop = 1) 
  LEFT JOIN `image` i ON (i.`id_image` = image_shop.`id_image`) 
- LEFT JOIN `image_lang` il ON (il.`id_image` = i.`id_image`) 
  LEFT JOIN `product_download` pd ON (pd.`id_product` = p.`id_product`) 
  INNER JOIN `category_product` cp ON (cp.`id_product` = p.`id_product` AND cp.`id_category` = 362) 
 WHERE (1 AND state = 1)
